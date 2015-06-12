2bc5537d5686708731d22883a204c864a1df07c8
Marlena Abraham <eleynta@gmail.com>
2015-06-12 17:00:25 -0400

    Testing lines
    Created a new line. Works pretty well. Next step: Connect end to mouse.

--- MindMap/Assets/Scripts/Nodes/NodeSerialized.cs
+++ MindMap/Assets/Scripts/Nodes/NodeSerialized.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum NodeState{Active, Inactive, Completed};
+
 [System.Serializable]
 public class NodeSerialized {
+	/* Add an enum for state*/
 	public string titleName = "New Node";
 	public string description = "New description";
 	public bool isSelected = false;
 	public int idNumber;
 	public int priority;
+
+	public NodeState currentState;
 
 	public float locationX;
 	public float locationY;
