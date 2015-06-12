865b52f2e43d886d4c68bba8c6719767cf4aaf87
Marlena Abraham <eleynta@gmail.com>
2015-06-12 16:22:20 -0400

    Beginning new project: Connections
    Added a new class ConnectionSerialized, some new values, etc.

---
+++ MindMap/Assets/Scripts/Nodes/ConnectionSerialized.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class ConnectionSerialized {
+	public string label;
+	public int thickness;
+	public bool isBold;
+	public bool isVisible;
+
+	public int rValue;
+	public int gValue;
+	public int bValue;
+}
--- MindMap/Assets/Scripts/Nodes/NodeSerialized.cs
+++ MindMap/Assets/Scripts/Nodes/NodeSerialized.cs
@@ -4,8 +4,10 @@ using System.Collections;
 [System.Serializable]
 public class NodeSerialized {
 	public string titleName = "New Node";
+	public string description = "New description";
 	public bool isSelected = false;
 	public int idNumber;
+	public int priority;
 
 	public float locationX;
 	public float locationY;
